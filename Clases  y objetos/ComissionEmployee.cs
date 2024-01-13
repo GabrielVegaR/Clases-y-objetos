a5aae26cc554b95cb68b5dce1651dd33b4ed0f82
gabriel <gabrielvegarosario@gmail.com>
2024-01-12 20:47:19 -0400

    Agregar archivos de proyecto.

---
+++ Clases  y objetos/ComissionEmployee.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases__y_objetos
+{
+    internal class ComissionEmployee
+    {
+        ////propiedades o atributos de un empleado por comision
+        //private string firstName;
+        //private string lastName;
+        //private int socialSecurityNumber;
+        //private decimal salary;
+
+        ////propiedades o menotos getters y setters
+        //public string FirstName
+        //{
+        //    get
+        //    {
+        //        return firstName;
+        //    }
+        //    set
+        //    {
+        //        firstName = value;
+        //    }
+        //}
+
+        //propiedades autogeneradas
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+
+        //atributos
+        private decimal grossSales;
+        private decimal commissionRate;
+
+        //propiedades para controlar los accesos a los atributos
+        public decimal GrossSales
+        {
+            get
+            {
+                return grossSales;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error, las ventas totales deben ser 0 o mas");
+                    return;
+                }
+                grossSales = value;
+            }
+        }
+
+        public decimal CommissionRate
+        {
+            get
+            {
+                return commissionRate;
+            }
+            set
+            {
+                if (value < 0 || value >= 1)
+                {
+                    Console.WriteLine("La tasa de comisiones debe ser menor que 1");
+                    return;
+                }
+                commissionRate = value;
+            }
+        }
+
+        //constructor
+        public ComissionEmployee(string firstName, string lastName, string socialSecurityNumber, decimal grossSales, decimal commissionRate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+            GrossSales = grossSales;
+            CommissionRate = commissionRate;
+        }
+
+        public ComissionEmployee(string firstName, string lastName, string socialSecurityNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+
+        //metodos para brindar los sevicios de clase
+        public decimal Earnings()
+        {
+            return commissionRate * grossSales;
+        }
+
+        public override string ToString()
+        {
+            return $"Empleado por comisión: {FirstName} {LastName}\n" +
+                $"Numero de seguridad social: {SocialSecurityNumber}\n" +
+                $"Ventas Brutas: {GrossSales:C}\n" +
+                $"Tasa de comisiones: {CommissionRate}\n" +
+                $"Ganancias del empleado: {Earnings():C}\n";
+        }
+    }
+}
---
+++ Clases  y objetos/EmployeeText.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Text.Json.Serialization;
+
+namespace Clases__y_objetos
+{
+    internal class EmployeeText
+    {
+        static void Main(string[] args)
+        {
+            //var empleado = new ComissionEmployee("juana", "reyes", "123456", 1000.00M, 0.06M);
+
+            //Console.WriteLine("Informacion de un empleado por comision\n");
+            //Console.WriteLine(empleado.ToString());
+
+            ComissionEmployee empleado = null;
+
+            while (true)
+            {
+                int opcion;
+                string firtsName;
+                string lastName;
+                string socialSecurityNumber;
+                decimal grossSales;
+                decimal commissionRate;
+
+                Console.WriteLine("1.- crear un empleado por comisión");
+                Console.WriteLine("2.- Ver datos del empleado");
+
+                Console.Write("seleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch(opcion)
+                {
+                    case 1:
+                        Console.Write("Dijite el nombre del empleado: ");
+                        firtsName = Console.ReadLine();
+                        Console.Write("Dijite el apellido del empleado: ");
+                        lastName = Console.ReadLine();
+                        Console.Write("Dijite el numero de seguridad social del empleado: ");
+                        socialSecurityNumber = Console.ReadLine();
+                        Console.Write("Dijita las ventas brutas del empleado: ");
+                        grossSales = decimal.Parse(Console.ReadLine());
+                        Console.Write("Dijite la tasa de comisines del empleado: ");
+                        commissionRate = decimal.Parse(Console.ReadLine());
+                        empleado = new ComissionEmployee(firtsName, lastName, socialSecurityNumber, grossSales, commissionRate);
+                        break;
+
+                    case 2:
+                        Console.WriteLine(empleado.ToString());
+                        break;
+
+                }
+            }
+        }
+    }
+}
